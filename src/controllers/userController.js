817b4054a3ae53221591331c14166d7294dfaf37
Matthew McKney <mrmckney@gmail.com>
2021-08-20 10:26:15 -0400

    added jwt token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,34 +1,45 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const {secret} = require('../../secrets')
 
 exports.createUser = (req, res) => {
     new User(req.body)
     .save()
-    .then(() => res.status(200).send({
-        message: "User Created",
-        status: 200
+    .then(user => {
+        const token = jwt.sign({user}, secret)
+        res.send({
+            message: "User created successfully",
+            status: 200,
+            user,
+            token
+        })
+    })
+    .catch(err => res.status(500).send({
+        message: err.message,
+        status: 500
     }))
-    .catch(err => res.status(500).send(err))
 }
 
 exports.loginUser = (req, res ) => {
     User.findOne({ username: req.body.username })
-    .then((userFound) => {
-        if (!userFound || userFound.password !== req.body.password) {
-          return res
-            .status(404)
-            .send({
-                message: "Login Successful",
-                status: 404
+    .then(user => {
+        if (!user || user.password !== req.body.password) {
+            return res.send({
+                message: 'Login Attempt Failed',
+                status: 401
             })
         }
-        if (userFound && userFound.password === req.body.password) {
-          res.status(200).send(userFound)
-        } else {
-          res.status(401).send({
-              message: "Username or password incorrect",
-              status: 401
-          })
-        }
+        if (user && user.password === req.body.password) {
+            let responseData = user
+            responseData.password = undefined
+            const token = jwt.sign({user}, secret)
+          res.status(200).send(res.send({
+            message: "User login successful",
+            status: 200,
+            user: responseData,
+            token
+        }))
+        } 
       })
       .catch(err => res.status(500).send(err))
 }
